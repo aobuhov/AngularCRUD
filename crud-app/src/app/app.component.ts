54666c7707e666a658bfbed181b1cdfd42dfa46c
Aleksei Obukhov <alexey.obuhov@gmail.com>
2023-05-08 21:50:49 +0300

    DI of dialog and linking with button

--- crud-app/src/app/app.component.ts
+++ crud-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {EmployeeAddEditComponent} from "./components/employee-add-edit/employee-add-edit.component";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crud-app';
+
+  constructor(private _dialog: MatDialog) {
+
+  }
+
+  openAddEditEmployeeForm() {
+    this._dialog.open(EmployeeAddEditComponent);
+  }
+
 }
