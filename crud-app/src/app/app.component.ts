9d56d17bcfe9ad813bea26ce89dcd8626e0c6251
Aleksei Obukhov <alexey.obuhov@gmail.com>
2023-05-08 23:14:32 +0300

    DI EmployeeService into app.component.ts add into EmployeeService getEmployeeList implement OnInit for AppComponent

--- crud-app/src/app/app.component.ts
+++ crud-app/src/app/app.component.ts
@@ -1,21 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
 import {EmployeeAddEditComponent} from "./components/employee-add-edit/employee-add-edit.component";
+import {EmployeeService} from "./services/employee.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crud-app';
 
-  constructor(private _dialog: MatDialog) {
+  constructor(private _dialog: MatDialog, private _employeeService: EmployeeService) {
 
+  }
+
+  ngOnInit() {
+    this.getEmployeeList();
   }
 
   openAddEditEmployeeForm() {
     this._dialog.open(EmployeeAddEditComponent);
+  }
+
+  getEmployeeList() {
+    this._employeeService.getEmployeeList().subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: console.log,
+      }
+    );
   }
 
 }
